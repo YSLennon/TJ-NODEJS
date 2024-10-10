9e28a0817f607dc11104d3db94cb2d0bef1abbbb
YSLennon <ysha0123@naver.com>
2024-10-10 21:21:30 +0900

    fs.promises 모듈 사용 연습

--- day2/fs.js
+++ day2/fs.js
@@ -1,45 +1,49 @@
 const fs = require('fs');
 
-fs.readdir('./', (err, files) => {
-    if(err){
-        console.log(err);
-        return;
-    }
-    console.log(files);
-    
-});
-
-fs.readFile('./fs.txt', (err, data) => {
-    if(err){
-        console.log(err);
-        return;
-    }
-    // console.log(data.toString());
-    
-})
-
-fs.readFile('./fs.txt','utf8', (err, data) => {
-    if(err){
-        console.log(err);
-        return;
-    }
-    // console.log(data.toString());
-    // console.log(data);
-    fs.writeFile('fs2.txt',data, (err) => {
-        if(err){
-            console.log(err);
-            return;
-        }
-        console.log('성공');
-    });  
-})
-
-// 이런 양식은 안되나?
-
-// async function fsRead(){
-//     try{
-//         const result = await fs.readFile('./fs.txt', 'utf8', )
-//     } catch(err){
+// fs.readdir('./', (err, files) => {
+//     if(err){
 //         console.log(err);
+//         return;
 //     }
-// }
+//     console.log(files);
+    
+// });
+
+// fs.readFile('./fs.txt', (err, data) => {
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     // console.log(data.toString());
+    
+// })
+
+// fs.readFile('./fs.txt','utf8', (err, data) => {
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     // console.log(data.toString());
+//     // console.log(data);
+//     fs.writeFile('fs2.txt',data, (err) => {
+//         if(err){
+//             console.log(err);
+//             return;
+//         }
+//         console.log('성공');
+//     });  
+// })
+
+// 이런 양식은 안되나? -> 되는구나? ㅋㅋ
+const fs2 = require('fs').promises;
+
+async function fsRead(){
+    try{
+        const result = await fs2.readFile('./fs.txt', 'utf8');
+        await fs2.writeFile('fs3.txt', result)
+        console.log('성공');
+    } catch(err){
+        console.log(err);
+    }
+}
+fsRead();
